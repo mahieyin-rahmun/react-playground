b23ce5b1f4406f0b16bbba37e34c5d31dc5e6308
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-04-28 21:56:01 +0600

    intro to states using constructors

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,9 @@ export default class Contact extends Component {
         let { name, email, phone } = this.props;
         return (
             <div className="mt-4">
-                <h5 className="alert alert-dark"> Name: {name.toUpperCase()}</h5>
+                <h5 className="alert alert-dark">
+                    Name: <strong> {name.toUpperCase()} </strong>
+                </h5>
                 <ul className="list-group">
                     <li className="list-group-item">
                         <strong>Email</strong>: {email}
