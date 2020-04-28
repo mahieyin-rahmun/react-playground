f577294134d7357c0140fac3239d9bcb2cc80a02
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-04-28 21:56:17 +0600

    intro to states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Components/Header';
-import Contact from "./Components/Contact";
+import Contacts from './Components/Contacts'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="container">
 				<Header title="Contact Manager" />
-				<Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-				<Contact name="Robert Downey Jr." email="robertd@gmail.com" phone="333-333-3333" />
+				<Contacts />
 			</div>
 		)
 	}
---
+++ src/Components/Contacts.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Contact from "./Contact"
+
+class Contacts extends Component {
+    constructor() {
+        super();
+        this.state = {
+            contacts: [
+                {
+                    id: 1,
+                    name: "John Doe",
+                    email: "jdoe@gmail.com",
+                    phone: "555-555-5555"
+                },
+                {
+                    id: 2,
+                    name: "Robert Downey Jr.",
+                    email: "robertd@gmail.com",
+                    phone: "333-333-3333"
+                },
+                {
+                    id: 3,
+                    name: "Luna Lovegood",
+                    email: "luna.lovegood@outlook.com",
+                    phone: "777-777-7777"
+                }
+            ]
+        }
+    }
+
+    render() {
+        const { contacts } = this.state;
+
+        return (
+            <div>
+                {contacts.map(contact => (
+                    <Contact
+                        key={contact.id}
+                        name={contact.name}
+                        email={contact.email}
+                        phone={contact.phone}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Contacts;
