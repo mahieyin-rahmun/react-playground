1858a800487519a2ce27119e4a864275e1183cd4
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-04-28 22:12:44 +0600

    integrating fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Header from './Components/Header';
 import Contacts from './Components/Contacts'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faSortDown)
 
 class App extends Component {
 	render() {
