6527fb067c30bf014dc2a3d842b25451e10b6179
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-04-30 08:49:41 +0600

    replaced class with className

--- src/Components/layouts/Header.js
+++ src/Components/layouts/Header.js
@@ -5,18 +5,18 @@ export default class Header extends Component {
 	render() {
 		let { title } = this.props;
 		return (
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<Link class="navbar-brand" to="/">{title}</Link>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<Link className="navbar-brand" to="/">{title}</Link>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item active">
+							<Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
 						</li>
-						<li class="nav-item">
-							<Link class="nav-link" to="/add">Add Contact</Link>
+						<li className="nav-item">
+							<Link className="nav-link" to="/add">Add Contact</Link>
 						</li>
-						<li class="nav-item">
-							<Link class="nav-link" to="/about">About</Link>
+						<li className="nav-item">
+							<Link className="nav-link" to="/about">About</Link>
 						</li>
 					</ul>
 				</div>
