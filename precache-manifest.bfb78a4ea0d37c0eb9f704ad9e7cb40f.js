381c9f7b9c7cf4bf1a185a600346134f6787f5c5
mahieyin-rahmun <mahieyin.rahmun@gmail.com>
2020-04-30 16:40:57 +0600

    Updates

---
+++ precache-manifest.bfb78a4ea0d37c0eb9f704ad9e7cb40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bbeef6fe902bc373b6d2810b09d7a3",
+    "url": "/react-playground/index.html"
+  },
+  {
+    "revision": "b3f2cad1e9089c6d6bb0",
+    "url": "/react-playground/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6c8ca0586f007c6c6810",
+    "url": "/react-playground/static/js/2.67dfb575.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-playground/static/js/2.67dfb575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f2cad1e9089c6d6bb0",
+    "url": "/react-playground/static/js/main.5081aafd.chunk.js"
+  },
+  {
+    "revision": "5659cad4cbc9cf45cc67",
+    "url": "/react-playground/static/js/runtime-main.c84832da.js"
+  }
+]);
